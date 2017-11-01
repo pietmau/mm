4af5f6ac7606aa24f31f14f305ea5683eb101244
Maurizio Pietrantuono <maurizio.pietrantuon@Mac2210.local>
2017-11-01 16:07:52 +0000

    Hide/show toolabr and nav bar

---
+++ mm/ScreenCapturer.swift
@@ -0,0 +1,11 @@
+//
+// Created by Maurizio Pietrantuono on 01/11/2017.
+// Copyright (c) 2017 Maurizio Pietrantuono. All rights reserved.
+//
+
+import Foundation
+
+class ScreenCapturer {
+    init(_ controller: ViewController) {
+    }
+}
---
+++ mm/saversharer/SaverSharer.swift
@@ -0,0 +1,9 @@
+//
+// Created by Maurizio Pietrantuono on 01/11/2017.
+// Copyright (c) 2017 Maurizio Pietrantuono. All rights reserved.
+//
+
+import Foundation
+
+class SaverSharer {
+}
